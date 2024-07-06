aadc97df4ce1fd6f1f356bbbcc5550a17f0b63fc
yagodanr <yagodanr@gmail.com>
2024-07-06 12:39:50 +0300

    Added navbar as sidebar

--- social_media/urls.py
+++ social_media/urls.py
@@ -4,7 +4,7 @@ from .views import MainPage, ChannelView, MyUserView
 
 
 urlpatterns = [
-    path("", MainPage.as_view(), name="posts"),
+    path("", MainPage.as_view(), name="main-page"),
     path("channel/<int:pk>", ChannelView.as_view(), name="channel"),
     path("user/<int:pk>", MyUserView.as_view(), name="user"),
     
