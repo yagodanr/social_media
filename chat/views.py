c4c80c8405d32c2d0ba061993b144783c15e610c
yagodanr <yagodanr@gmail.com>
2024-07-19 01:10:35 +0300

    Added message sending

--- chat/views.py
+++ chat/views.py
@@ -1,15 +1,31 @@
+from typing import Any
+from django.http import HttpRequest, HttpResponse
 from django.views.generic import DetailView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
 
-from .models import Chat
+
+from .models import Chat, Message
 
 # Create your views here.
+class ChatListView(TemplateView, LoginRequiredMixin):
+    template_name = "chat/chats_list.html"
 
 class ChatView(DetailView, LoginRequiredMixin):
     model = Chat
     template_name = "chat/chat_open.html"
+    
+    def post(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        if not request.user.is_authenticated:
+            return redirect("login")
+        
+        if "content" in request.POST:
+            chat = self.get_object()
+            
+            msg = Message(sender=request.user, content=request.POST["content"], chat=chat)
+            msg.save()
+            
+            
+        
+        return self.get(request, *args, **kwargs)
 
-
-class ChatListView(TemplateView, LoginRequiredMixin):
-    model = Chat
-    template_name = "chat/chats_list.html"
